85026f35f2c486acf05522c54e8c0547ccfa2b94
Akhil Kammila <akhilkammila@gmail.com>
2022-08-27 18:56:20 -0400

    posts requests working on postman, passing values to ML model

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-# Setting up flask:
-# https://stackoverflow.com/questions/31252791/flask-importerror-no-module-named-flask
-# https://github.com/miguelgrinberg/microblog/issues/244
-
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+import flask
 import pickle
 import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
@@ -17,13 +15,19 @@ def home():
     return 'hi'
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    d = {'Area': ['India'], 'Item': ['Potatoes'], 'Year': [2000], 'average_rain_fall_mm_per_year': [
+
+    item = [x for x in request.form.values()]
+    country = request.args.get('country')
+    year = 2020
+
+    d = {'Area': ['India'], 'Item': ['Potatoes'], 'Year': [2020], 'average_rain_fall_mm_per_year': [
         1000.0], 'pesticides_tonnes': [127585.0], 'avg_temp': [25.32]}
     test = pd.DataFrame(data=d)
-    pipe.predict(test)
-    return 'predict'
+    prediction = pipe.predict(test)
+    print(prediction)
+    return [1, 2]
 
 
 if __name__ == "__main__":
